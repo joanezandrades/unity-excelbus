36dc53cc47ac718f5f0db315f2ec24915c9fa0e6
joanezandrades <box@unitycode.tech>
2020-03-25 19:24:58 -0300

    First Commit

---
+++ unity-excelbus.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin name: UnityExcelbus
+ * Plugin URI: https://github.com/joanezandrades/unity-excelbus
+ * Description: Plugin que faz upload de arquivos excel e transforma os dados da planilha em publicações/atualizações
+ * da tabela dos horários de ônibus.
+ * Version: 0.1.0
+ * Author: JA93
+ * Author URI: http://unitycode.tech
+ * Text domain: unex
+ * License: GPL2
+*/
+if (!defined('ABSPATH'))
+{
+    exit;
+}
